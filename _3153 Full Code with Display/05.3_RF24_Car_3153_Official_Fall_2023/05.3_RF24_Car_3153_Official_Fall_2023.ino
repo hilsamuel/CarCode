c603e5ea42766f8ca657f46fa0af787247c378f5
hilsamuel <164909362+hilsamuel@users.noreply.github.com>
2024-03-25 11:05:40 -0500

    Add files via upload

---
+++ _3153 Full Code with Display/05.3_RF24_Car_3153_Official_Fall_2023/05.3_RF24_Car_3153_Official_Fall_2023.ino
@@ -0,0 +1,217 @@
+/**********************************************************************
+  Filename    : RF24_Remote_Car.ino
+  Product     : Freenove 4WD Car for UNO
+  Description : A RF24 Remote Car.
+  Auther      : www.freenove.com
+  Modification: 2020/11/27
+**********************************************************************/
+#include "Freenove_4WD_Car_for_Arduino.h"
+#include "RF24_Remote.h"
+
+//RDD Add small display screen and code to set it up and use it
+#include <Wire.h>
+#include "SSD1306AsciiWire.h"
+#define I2C_ADDRESS 0x3C
+SSD1306AsciiWire oled;
+
+void setupScreen() {
+  Wire.begin();
+  Wire.setClock(400000L);
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.clear();
+  oled.setFont(fixed_bold10x15);
+}
+
+void clearScreen() {
+  oled.clear();
+}
+
+void screenPrint(int row, int col, char* value) {
+  oled.clearField((col - 1) *11, (row - 1) * 2, 12);    
+  oled.print(value);
+}
+
+
+//RDD Add code to use the servo
+#include <Servo.h>
+Servo servo1;
+
+#define NRF_UPDATE_TIMEOUT    1000
+
+//RDD add modes
+#define MODE_STOP 0
+#define MODE_MANUAL 1
+#define MODE_AUTO 2
+
+#define AUTO_MODE_STOP 0
+#define AUTO_MODE_FORWARD 1
+#define AUTO_MODE_BACKWARD 2
+#define AUTO_MODE_RIGHTTURN 3
+#define AUTO_MODE_LEFTTURN 4
+#define AUTO_MODE_SERVO_POS 5
+#define AUTO_MODE_SERVO_NEG 6
+
+#define SERVO_STOP 91
+
+int mode, automode, start_time, current_time, delta_time;
+
+
+u32 lastNrfUpdateTime = 0;
+
+void setup() {
+  pinsSetup();
+  servo1.attach(PIN_SERVO);  //RDD
+  if (!nrf24L01Setup()) {
+    alarm(3, 2);
+  }
+  alarm(2, 1);  //RDD
+  mode = MODE_MANUAL;  //RDD
+
+  setupScreen();   //RDD  Setup screen and print the group number and battery voltage
+  screenPrint(1,3,"Group 11");
+  getBatteryVoltage();  
+  char buffer[6];
+  dtostrf(batteryVoltage,4,2,buffer);
+  screenPrint(2,1,buffer);  
+  screenPrint(2,6,"Volts");
+  screenPrint(3,1,"Mode: Manual");
+
+}
+
+void loop() {
+
+  if (getNrf24L01Data()) {  //if radio data was received
+    clearNrfFlag();
+
+    getBatteryVoltage();  
+    char buffer[6];
+    dtostrf(batteryVoltage,4,2,buffer);
+    screenPrint(2,1,buffer);  
+    screenPrint(2,6,"Volts");
+  
+    if (mode == MODE_STOP  ) {
+      if (nrfDataRead[7] == 0) {
+        mode = MODE_AUTO;
+        screenPrint(3,1,"Mode: Auto");        
+              
+        automode = AUTO_MODE_STOP;
+        alarm(1, 1);     
+        start_time = millis();
+        //Serial.println(start_time);
+        servo1.write(SERVO_STOP);
+      }  
+      if (nrfDataRead[4] == 0) {
+        mode = MODE_MANUAL;
+        screenPrint(3,1,"Mode: Manual"); 
+        screenPrint(4,1," ");   
+        alarm(2, 1);  
+        servo1.write(SERVO_STOP);
+      }  
+    }
+
+    if (mode == MODE_AUTO){
+      current_time = millis();
+      delta_time = current_time - start_time;
+
+      if (delta_time > 15000) {
+        mode = MODE_MANUAL;
+        screenPrint(3,1,"Mode: Manual"); 
+        screenPrint(4,1," ");          
+        alarm(2, 1);            
+      } else {
+        autonomous(delta_time);
+      }
+    }
+
+    if (mode == MODE_MANUAL  ) {
+      //the joystick controls the servo positions
+      if (nrfDataRead[4] == 0) {
+        mode = MODE_STOP;
+        screenPrint(3,1,"Mode: Stop"); 
+        screenPrint(4,1," ");   
+        alarm(3, 1);  
+        servo1.write(SERVO_STOP);
+      }
+      manual();
+    }
+
+    lastNrfUpdateTime = millis();
+  }
+
+  if (millis() - lastNrfUpdateTime > NRF_UPDATE_TIMEOUT) {
+    lastNrfUpdateTime = millis();
+    resetNrfDataBuf();
+    updateCarActionByNrfRemote();
+  }
+}
+
+
+void manual() {
+  updateCarActionByNrfRemote();  //make the car go
+
+  //handle the servo
+  float servospeed = nrfDataRead[0] /1023.0;  // 1023.0 not 1023 
+  //Serial.println(servospeed);
+  if (nrfDataRead[5] == 0 ) {
+    servo1.write( int(91 + servospeed * 36));  // servo reaches max  positive speed at 127
+  }  else if (nrfDataRead[6] == 0 ) {
+    servo1.write(int(91 - servospeed * 32));  // servo reaches max  negative speed at 63
+  } else {
+    servo1.write(91); //servo stops at 91
+  }
+
+}
+
+
+void autonomous(int now) {
+  int endtime_prev = 0;
+  //                    mode        current_time  start   duration
+  endtime_prev = do_auto(AUTO_MODE_FORWARD,   now,    0,      1500);
+  endtime_prev = do_auto(AUTO_MODE_STOP,      now,  endtime_prev, 1000);
+  endtime_prev = do_auto(AUTO_MODE_BACKWARD,  now,  endtime_prev, 1500);
+  endtime_prev = do_auto(AUTO_MODE_STOP,      now,  endtime_prev, 1000);
+  endtime_prev = do_auto(AUTO_MODE_RIGHTTURN, now,  endtime_prev, 1500);
+  endtime_prev = do_auto(AUTO_MODE_STOP,      now,  endtime_prev, 1000);
+  endtime_prev = do_auto(AUTO_MODE_LEFTTURN,  now,  endtime_prev, 1500);
+  endtime_prev = do_auto(AUTO_MODE_STOP,      now,  endtime_prev, 1000);
+  endtime_prev = do_auto(AUTO_MODE_SERVO_POS, now,  endtime_prev, 1500);
+  endtime_prev = do_auto(AUTO_MODE_STOP,      now,  endtime_prev, 1000);
+  endtime_prev = do_auto(AUTO_MODE_SERVO_NEG, now,  endtime_prev, 1500);
+  endtime_prev = do_auto(AUTO_MODE_STOP,      now,  endtime_prev, 500);
+}
+
+int do_auto(int automode, int current_time,  int start_time, int duration) {
+
+  int stop_time = start_time + duration;
+
+  if (current_time > start_time  &&   current_time < stop_time)
+
+  switch (automode) {
+  case AUTO_MODE_STOP:
+    motorRun(0, 0);
+    servo1.write(90);
+      break;
+  case AUTO_MODE_FORWARD:
+    motorRun(150, 150);
+      break;
+  case AUTO_MODE_BACKWARD:
+    motorRun(-150, -150);
+      break;
+  case AUTO_MODE_RIGHTTURN:
+      motorRun(200, -200);
+      break;
+  case AUTO_MODE_LEFTTURN:
+      motorRun(-185, 185);
+      break;
+  case AUTO_MODE_SERVO_POS:
+      motorRun(0, 0);
+      servo1.write(180);
+      break;
+  case AUTO_MODE_SERVO_NEG:
+      motorRun(0, 0);
+      servo1.write(0);
+      break;
+  }  
+  return stop_time;
+}
+
---
+++ _3153 Full Code with Display/05.3_RF24_Car_3153_Official_Fall_2023/RF24_Remote.cpp
@@ -0,0 +1,97 @@
+#include "RF24_Remote.h"
+
+RF24 radio(PIN_SPI_CE, PIN_SPI_CSN);
+
+    // Radio Address: each team should choose a unique address string
+    // I suggest "TM0xx" where xx is your team number  ex:  TM001, TM014
+const byte addresses[6] = "TM011";
+    // Radio Channel: each team should choose a unique channel number between 20 and 127
+    // I suggest you use:   (TeamNumber * 5) + 15  ex:  team 4 would use channel 35
+const int RF24channel = 70;  //each team should choose a unique channel
+
+int nrfDataRead[8];
+bool nrfComplete = false;
+
+bool nrf24L01Setup() {
+  // NRF24L01
+  if (radio.begin()) {                      // initialize RF24
+    radio.setPALevel(RF24_PA_MAX);      // set power amplifier (PA) level
+    radio.setDataRate(RF24_1MBPS);      // set data rate through the air
+    radio.setRetries(0, 15);            // set the number and delay of retries
+    radio.openWritingPipe(addresses);   // open a pipe for writing
+    radio.openReadingPipe(1, addresses);// open a pipe for reading
+    radio.setChannel(RF24channel);    
+    radio.startListening();             // start monitoringtart listening on the pipes opened
+
+    FlexiTimer2::set(20, 1.0 / 1000, checkNrfReceived); // call every 20 1ms "ticks"
+    FlexiTimer2::start();
+
+    return true;
+  }
+  return false;
+}
+
+void checkNrfReceived() {
+  delayMicroseconds(1000);
+  if (radio.available()) {             // if receive the data
+    while (radio.available()) {         // read all the data
+      radio.read(nrfDataRead, sizeof(nrfDataRead));   // read data
+    }
+    nrfComplete = true;
+    return;
+  }
+  nrfComplete = false;
+}
+
+bool getNrf24L01Data()
+{
+  return nrfComplete;
+}
+
+void clearNrfFlag() {
+  nrfComplete = 0;
+}
+
+void updateCarActionByNrfRemote() {
+  
+  float speed = nrfDataRead[1] /1023.0;  // 1023.0 not 1023 
+
+  int rightturn = nrfDataRead[2] - 512;
+  int forward = -(nrfDataRead[3] - 512);
+  int leftmotor, rightmotor;
+
+  if ( nrfDataRead[7] == 0) {
+    forward = -forward;
+  }
+
+  rightturn=rightturn * speed;
+  //forward = forward * speed;
+
+  leftmotor = (forward + rightturn) / 2;
+  rightmotor = (forward - rightturn) / 2;
+    
+  motorRun(leftmotor, rightmotor);
+
+  if (nrfDataRead[4] == 0) {
+    setBuzzer(true);
+  }
+  else {
+    setBuzzer(false);
+  }
+}
+
+void resetNrfDataBuf() {
+  nrfDataRead[0] = 0;
+  nrfDataRead[1] = 0;
+  nrfDataRead[2] = 512;
+  nrfDataRead[3] = 512;
+  nrfDataRead[4] = 1;
+  nrfDataRead[5] = 1;
+  nrfDataRead[6] = 1;
+  nrfDataRead[7] = 1;
+}
+
+u8 updateNrfCarMode() {
+  // nrfDataRead [5 6 7] --> 111
+  return ((nrfDataRead[5] == 1 ? 1 : 0) << 2) | ((nrfDataRead[6] == 1 ? 1 : 0) << 1) | ((nrfDataRead[7] == 1 ? 1 : 0) << 0);
+}
